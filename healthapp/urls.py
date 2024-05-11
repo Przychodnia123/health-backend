4fc4a8e6567a076f0abedb33e141469ec4fcc279
JolantaWro <jolanta.woronowska@gmail.com>
2024-05-11 20:33:24 +0200

    dashboard user

--- healthapp/urls.py
+++ healthapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("login/", obtain_auth_token, name="login"),
     path("logout/", views.logout_user, name="logout"),
     path("register/", views.user_register_view, name="register"),
+    path("dashboard/", views.Dashboard.as_view())
 ]
